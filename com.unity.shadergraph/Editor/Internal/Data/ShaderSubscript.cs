935cfb57bba2579ed445d44aea72426f4aff8cae
Alex Lindman <alexandral@unity3d.com>
2019-09-18 14:02:00 -0700

    rough pass shader subscript

---
+++ com.unity.shadergraph/Editor/Internal/Data/ShaderSubscript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Rendering;
+
+namespace UnityEditor.ShaderGraph.Internal
+{
+    public class ShaderStruct
+    {
+        public string name;
+        public ShaderSubscript[] subscripts;
+    }
+    
+    public class ShaderSubscript
+    {
+        public static ShaderValueType concreteType;
+        public static string name;
+        public static string semantic;
+        public static string conditional;
+        public static string generatedType;
+        public enum SubscriptType 
+        {
+            Static,
+            Optional,
+            Generated
+        }
+        public static SubscriptType subscriptType;
+        public static void Create(ShaderValueType valueType, string subscriptName, string subscriptSemantic = "", string subscriptConditional = "")
+        {
+            subscriptType = SubscriptType.Static;
+            concreteType = valueType;
+            name = subscriptName;
+            semantic = subscriptSemantic;
+            conditional = subscriptConditional;
+        }
+
+        public static void CreateOptional(ShaderValueType valueType, string subscriptName, string subscriptSemantic = "", string subscriptConditional = "")
+        {
+            subscriptType = SubscriptType.Optional;
+            concreteType = valueType;
+            name = subscriptName;
+            semantic = subscriptSemantic;
+            conditional = subscriptConditional;
+        }
+
+        public static void CreateGenerated(string valueType, string subscriptName, string subscriptSemantic = "", string subscriptConditional = "")
+        {
+            subscriptType = SubscriptType.Generated;
+            generatedType = valueType;
+            name = subscriptName;
+            semantic = subscriptSemantic;
+            conditional = subscriptConditional;
+        }
+    }
+}
